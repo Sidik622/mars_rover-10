f76b1bcf63dc94489f4a4b410e4314e8d3c676ae
Sidik622 <110263995+Sidik622@users.noreply.github.com>
2023-10-05 02:02:13 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,6 +11,7 @@ input.onButtonPressed(Button.B, function () {
 music.play(music.createSoundExpression(WaveShape.Square, 5000, 0, 255, 0, 2000, SoundExpressionEffect.Vibrato, InterpolationCurve.Linear), music.PlaybackMode.UntilDone)
 music.play(music.createSoundExpression(WaveShape.Square, 3332, 3815, 255, 0, 2000, SoundExpressionEffect.Vibrato, InterpolationCurve.Logarithmic), music.PlaybackMode.UntilDone)
 basic.forever(function () {
+    serial.writeLine("" + (sparkbitI.irTransmitterIsReceived(SparkbitInPort.Input2, SparkbitInPort.Input3)))
     while (sparkbitI.lightSensorComparePercent(SparkbitInPort.Input1, SparkbitLogic.NEQ, 0)) {
         if (sparkbitI.irTransmitterIsReceived(SparkbitInPort.Input2, SparkbitInPort.Input2)) {
             sparkbitO.setLightModule(SparkbitOutPort.Output3, SparkbitColor.Green, 100)
